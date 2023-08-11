66870d4ed7a29cc53b38756a941158d9b1187884
Thomas Béguin <thom.beguin@gmail.com>
2023-08-11 17:23:45 +0200

    login page init and adding boostrap

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 const express = require('express');
 const app = express();
 const port = 3003;
+const middleware = require('./middleware');
 
-const server = app.listen(port, () => { console.log(`Listening on port: ${port}`)})
+const server = app.listen(port, () => { console.log(`Listening on port: ${port}`)});
+
+app.set("view engine", "pug");
+// When you need a view, go to the views folder
+app.set("views", "views");
+
+const loginRoute = require('./routes/loginRoutes');
+
+app.use("/login", loginRoute);
+
+app.get("/", middleware.requireLogin, (req, res, next) => {
+
+  const payload = {
+    pageTitle: "Home"
+  }
+  res.status(200).render("home", payload);
+})
---
+++ routes/loginRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+
+app.set("view engine", "pug");
+
+// When you need a view, go to the views folder
+app.set("views", "views");
+
+router.get("/", (req, res, next) => {
+  res.status(200).render("login");
+})
+
+router.post("/", (req, res, next) => {
+})
+
+module.exports = router;
