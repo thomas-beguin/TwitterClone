1d3039b96767fcb917b25989c2713eca359961ea
Thomas Béguin <thom.beguin@gmail.com>
2023-08-17 17:26:26 +0200

    implement validation for registration

--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const router = express.Router();
 const bodyParser = require('body-parser');
+const User = require('../schemas/UserSchema');
 
 app.set("view engine", "pug");
 app.set("views", "views"); // When you need a view, go to the views folder
@@ -12,17 +13,43 @@ router.get("/", (req, res, next) => {
   res.status(200).render("register");
 })
 
-router.post("/", (req, res, next) => {
+router.post("/", async (req, res, next) => {
   const firstName = req.body.firstName.trim();
   const lastName = req.body.lastName.trim();
   const username = req.body.username.trim();
+  const email = req.body.email.trim();
   const password = req.body.password;
 
   const payload = req.body;
 
-  if (firstName && lastName && username && password) {
+  if (firstName && lastName && username && email && password) {
+    const user = await User.findOne({
+      $or : [
+          { username: username },
+          { email: email }
+        ]
+    })
+    .catch((error) => {
+      console.log(error);
+      payload.errorMessage = "Something went wrong";
+      res.status(200).render('register', payload);
+    })
 
-  } else {
+    if (user === null) {
+      // No user found
+    } else {
+      // User found
+      if (email == user.email) {
+        payload.errorMessage = "Email already in use";
+      }
+      else {
+        payload.errorMessage = "Username already in use";
+      }
+      res.status(200).render('register', payload);
+    }
+
+  }
+  else {
     payload.errorMessage = "Make sure each field has a value";
     res.status(200).render('register', payload);
   }
